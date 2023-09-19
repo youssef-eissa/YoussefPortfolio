5eb27e967ec9165e0de47977d6e19a5df2281c10
youssef-eissa <youssefeissa1812@gmail.com>
2023-09-19 04:32:59 +0300

    updated deployment

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,7 +6,6 @@ import { useInView } from 'react-intersection-observer';
 import { useAnimation } from 'framer-motion';
 import { useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { render } from '@testing-library/react';
 
 function Projects() {
     const { ref, inView } = useInView({
@@ -40,8 +39,8 @@ return (
         className=' my-5 projects row'>
         <h1 className='col-12 my-5 title text-center'>My <span style={{color:"coral"}}>Projects</span></h1>
             <div className=' projects-container p-0 d-flex row-gap-5 justify-content-md-between justify-content-center flex-wrap'>
-                
-                <Link style={{textDecoration:"none"}} to='https://youssef-eissa.github.io/shopzone/'  className='col-md-5 col-11 project-box rounded overflow-hidden d-flex flex-column'>
+
+                <Link reloadDocument style={{textDecoration:"none"}} to='https://youssef-eissa.github.io/shopzone/'  className='col-md-5 col-11 project-box rounded overflow-hidden d-flex flex-column'>
                 <div className='theImg overflow-hidden col-12'>
                     <img className='img-fluid  h-100 w-100' src={require('../assest/shopzone.png') } alt='proj-img' />
                 </div>
@@ -53,7 +52,7 @@ return (
                 </div>
             </Link>
 
-            <Link to='https://youssef-eissa.github.io/Audiophile-e-commerce/' style={{textDecoration:"none"}} className='col-md-5 col-11 rounded overflow-hidden project-box d-flex flex-column'>
+            <Link reloadDocument to='https://youssef-eissa.github.io/Audiophile-e-commerce/' style={{textDecoration:"none"}} className='col-md-5 col-11 rounded overflow-hidden project-box d-flex flex-column'>
                 <div className='theImg overflow-hidden col-12'>
                     <img className='img-fluid h-100 w-100' src={require('../assest/ecom.jpeg') } alt='proj-img' />
                 </div>
@@ -63,17 +62,17 @@ return (
                     <span style={{textDecoration:"none",fontSize:"16px",color:"coral"}} to='https://youssef-eissa.github.io/Audiophile-e-commerce/'>Show More <KeyboardDoubleArrowRightIcon/> </span>
                 </div>
             </Link>
-            <Link to='https://youssef-eissa.github.io/PayApi-Project' style={{textDecoration:"none"}} className='col-md-5 col-11 project-box rounded overflow-hidden d-flex flex-column project-box'>
+            <Link reloadDocument to='https://youssef-eissa.github.io/PayApi-Project' style={{textDecoration:"none"}} className='col-md-5 col-11 project-box rounded overflow-hidden d-flex flex-column project-box'>
                 <div className='theImg overflow-hidden col-12'>
                     <img className='img-fluid h-100 w-100' src={require('../assest/payapi.jpeg') } alt='proj-img' />
                 </div>
                 <div style={{ backgroundColor: "#dbd5d5",color:"black"}} className='col-12 p-3 text'>
                     <h3 style={{ color: "coral" }} className='col-12'>PayAPI multi-page website</h3>
                     <p className='col-12' >A 4 multi-page Website that view the optimal layout for each page depending on their device's screen size.</p>
-                    <span style={{textDecoration:"none",fontSize:"16px",color:"coral"}} to='https://youssef-eissa.github.io/PayApi-Project'>Show More <KeyboardDoubleArrowRightIcon/> </span>
+                    <span  style={{textDecoration:"none",fontSize:"16px",color:"coral"}} to='https://youssef-eissa.github.io/PayApi-Project'>Show More <KeyboardDoubleArrowRightIcon/> </span>
                 </div>
             </Link>
-            <Link   to='https://youssef-eissa.github.io/multi-step-app-form' style={{textDecoration:"none"}} className='col-md-5 col-11 project-box rounded overflow-hidden d-flex flex-column'>
+            <Link reloadDocument  to='https://youssef-eissa.github.io/multi-step-app-form' style={{textDecoration:"none"}} className='col-md-5 col-11 project-box rounded overflow-hidden d-flex flex-column'>
                 <div className='theImg overflow-hidden col-12'>
                     <img className='img-fluid h-100 w-100' src={require('../assest/form.webp') } alt='proj-img' />
                 </div>
@@ -85,7 +84,7 @@ return (
             </Link>
         </div>
         <div className='col-12 d-flex my-5 justify-content-center'>
-            <Link target='_blank' to='https://github.com/youssef-eissa?tab=repositories' className='link col-md-2 col-11 rounded py-2 text-center'>See More</Link>
+            <Link reloadDocument target='_blank' to='https://github.com/youssef-eissa?tab=repositories' className='link col-md-2 col-11 rounded py-2 text-center'>See More</Link>
         </div>
     </motion.div>
     </div>
